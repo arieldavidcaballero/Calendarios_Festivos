7e8f24d2a645a5464ef0644520b7eb2009d6f366
saraycaceres12 <160681153+saraycaceres12@users.noreply.github.com>
2025-05-18 15:16:06 -0500

    Delete parcial/api directory

--- parcial/api/infraestructura/src/main/java/repositorios/FestivoRepository.java
+++
@@ -1,7 +0,0 @@
-package parcial.api.infraestructura.repositorios;
-
-import parcial.api.dominio.entidades.Festivo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FestivoRepository extends JpaRepository<Festivo, Long> {
-}
